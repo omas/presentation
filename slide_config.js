8baea46efb6e930aefd73a4f076dc10e644fd9dc
Omas Naohiko <omas@std.it-college.ac.jp>
2014-07-07 11:15:49 +0900

    template

---
+++ slide_config.js
@@ -0,0 +1,40 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: '職業講話@宜野座高校<br>コンピュータのお仕事',
+    subtitle: 'Computer Programming',
+    eventInfo: {
+      title: '職業講話',
+      date: '2014-06-20'
+    },
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/favicon.ico',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Omas Naohiko',
+    company: 'IT Specialist Course Instructor<br>IT College Okinawa',
+//    gplus: 'http://plus.google.com/omas',
+    twitter: '@omas_',
+    www: 'http://www.it-college.ac.jp',
+    github: 'http://github.com/omas'
+  }/*, {
+    name: 'Second Name',
+    company: 'Job Title, Google',
+    gplus: 'http://plus.google.com/omas.naohiko',
+    twitter: '@omas_',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  }*/]
+};
+
